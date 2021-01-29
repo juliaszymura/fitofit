ddeffb03f48e34a6d757d748ae7d48b6451b06ea
Julia Szymura <jeszymura@gmail.com>
2021-01-29 14:08:51 +0100

    add generate test exercises dates

--- tests/exercisesApi.test.js
+++ tests/exercisesApi.test.js
@@ -2,30 +2,33 @@ const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
 const storage = require("../utils/storage");
+const moment = require("moment");
+const groupByDay = require("../utils/groupByDay");
 
-describe("Exercise API", () => {
+describe("Exercises API", () => {
   const initialExercises = [
     {
-      date: "2021-01-03T20:56:59.806Z",
+      date: moment().subtract(2, "months").toISOString(),
       distance: 6.34,
     },
     {
-      date: "2021-01-12T21:56:59.806Z",
+      date: moment().subtract(7, "days").toISOString(),
       distance: 16.1,
     },
     {
-      date: "2021-01-17T21:56:59.806Z",
+      date: moment().subtract(7, "days").toISOString(),
       distance: 34.68,
     },
     {
-      date: "2021-01-27T20:56:59.806Z",
+      date: moment().toISOString(),
       distance: 1.23,
     },
     {
-      date: "2021-01-27T21:56:59.806Z",
+      date: moment().toISOString(),
       distance: 10.01,
     },
   ];
+
   beforeEach(() => {
     storage.clear();
     initialExercises.forEach((exercise) => storage.add(exercise));
@@ -49,12 +52,10 @@ describe("Exercise API", () => {
 
   describe("GET /api/exercises/grouped/current-month - exercises from current month grouped by day", () => {
     const path = "/api/exercises/grouped/current-month";
-    const groupedByDay = {
-      "2021-01-03": 6.34,
-      "2021-01-12": 16.1,
-      "2021-01-17": 34.68,
-      "2021-01-27": 11.24,
-    };
+    const lastMonth = initialExercises
+      .filter((ex) => moment(ex.date).year() === moment().year())
+      .filter((ex) => moment(ex.date).month() === moment().month());
+    const groupedLastMonth = groupByDay(lastMonth);
 
     test("responds with 200 status code", async () => {
       await api.get(path).expect(200);
@@ -65,7 +66,7 @@ describe("Exercise API", () => {
     });
 
     test("responds with exercises from current month grouped by day", async () => {
-      await api.get(path).expect(groupedByDay);
+      await api.get(path).expect(groupedLastMonth);
     });
   });
 
@@ -138,6 +139,13 @@ describe("Exercise API", () => {
         .post(path)
         .set("accept", "application/json")
         .send({ start: "hdfjdhfjdfshf", end: points.end })
+        .expect(400)
+        .expect(errorMessage);
+
+      await api
+        .post(path)
+        .set("accept", "application/json")
+        .send({ start: points.start, end: 1 })
         .expect(400)
         .expect(errorMessage);
     });
