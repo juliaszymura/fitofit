0cdcd10134e98c240e6950e32eee66cd036e3561
Julia Szymura <jeszymura@gmail.com>
2021-03-08 23:41:31 +0100

    migrate storage to postgres

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("express-async-errors");
 const morgan = require("morgan");
-const storage = require("./utils/storage");
+// const storage = require("./utils/storage");
 const calculateDistance = require("./utils/calculateDistance");
 const { unknownEndpoint, errorHandler } = require("./utils/middleware");
 const groupByDay = require("./utils/groupByDay");
@@ -12,23 +12,30 @@ const app = express();
 app.use(express.json());
 if (process.env.NODE_ENV !== "test") app.use(morgan("dev"));
 
-app.get("/api/exercises", (req, res) => {
-  db.query("SELECT * FROM exercises", (err, res) => {
-    if (err) {
-      console.log(err.stack);
-    } else {
-      console.log(res.rows);
-    }
-  });
-  const exercises = storage.read().exercises;
-  res.json(exercises);
+app.get("/api/exercises", async (req, res) => {
+  let response;
+  try {
+    response = await db.query("SELECT * FROM exercises");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+  res.json(response.rows);
 });
 
-app.get("/api/exercises/grouped/current-month", (req, res) => {
-  const exercises = storage.read().exercises;
+app.get("/api/exercises/grouped/current-month", async (req, res) => {
+  let response;
+  try {
+    response = await db.query("SELECT * FROM exercises");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
   const currentMonth = new Date().toISOString().slice(0, 7);
   const regex = new RegExp(currentMonth);
-  const exercisesCurrentMonth = exercises.filter((ex) => regex.test(ex.date));
+  const exercisesCurrentMonth = response.rows
+    .filter((ex) => regex.test(ex.date.toISOString()))
+    .map((ex) => ({ date: ex.date.toISOString(), distance: ex.distance }));
   res.json(groupByDay(exercisesCurrentMonth));
 });
 
@@ -38,9 +45,18 @@ app.post("/api/exercises", async (req, res) => {
     res.status(400).json({ error: "missing start or end address" });
   } else {
     const distance = await calculateDistance(body.start, body.end);
-    const exercise = { date: new Date().toISOString(), distance };
-    storage.add(exercise);
-    res.status(201).json(exercise);
+    const date = new Date().toISOString();
+    try {
+      await db.query({
+        name: "add-exercise",
+        text: "INSERT INTO exercises (date, distance) VALUES ($1, $2)",
+        values: [date, distance],
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+    res.status(201).json({ date, distance });
   }
 });
 
--- db/index.js
+++ db/index.js
@@ -1,7 +1,15 @@
 const { Pool } = require("pg");
-const pool = new Pool();
+const testConfig = {
+  user: "fitofit",
+  password: "Pass2021!",
+  database: "fitofit",
+  port: 5433,
+};
+
+const pool =
+  process.env.NODE_ENV !== "test" ? new Pool() : new Pool(testConfig);
+
 module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback);
-  },
+  query: (text, params) => pool.query(text, params),
+  end: () => pool.end(),
 };
